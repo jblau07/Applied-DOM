bb641593aff34fb5dd1d72a1fa6080f1e450e0e6
jblau07 <jlau7248@gmail.com>
2018-01-10 15:53:31 -1000

    button 7 click works

--- js/app.js
+++ js/app.js
@@ -131,7 +131,9 @@
    * using the Function prototype method: call
    * to set the context to the correct object
    */
-
+  btn7.addEventListener("click", () =>{
+    setMyLightGreen.call(btn7);
+  });
 
   /*
    * Add a click event listener to btn8
